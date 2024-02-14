84f8ed609893bdb2cb6c7915301ba638994e0fa5
Rabi Ahmed <ahmedrabi8@gmail.com>
2024-02-14 18:10:44 +0500

    env

--- src/Environment/environment.js
+++ src/Environment/environment.js
@@ -4,9 +4,22 @@
 //     url:process.env.REACT_APP_APP_URL,
 // }
 
+// const Constants = {
+//   authUrl: "https://stage-api.etows.app:8443",
+//   apiUrl: "https://stage-api.etows.app:9000",
+//   url: "http://127.0.0.1:3000",
+// };
+
+// stage
+// const Constants = {
+//   authUrl: "https://stage-api.etows.app:8443",
+//   apiUrl: "https://stage-api.etows.app:9000",
+//   url: "https://stage-console.etows.app",
+// };
+
 const Constants = {
-  authUrl: "https://stage-api.etows.app:8443",
-  apiUrl: "https://stage-api.etows.app:9000",
+  authUrl: "http://192.168.0.145:8080",
+  apiUrl: "http://192.168.0.145:9000",
   url: "http://127.0.0.1:3000",
 };
 
--- src/index.js
+++ src/index.js
@@ -1,87 +1,96 @@
 // import { createRoot } from 'react-dom/client';
 // import Index from 'views/Index';
-import React, { useContext, useEffect,useState,useCallback } from 'react'
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect, useHistory } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  useHistory,
+} from "react-router-dom";
 // import { useLocation } from 'react-router-dom';
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
-import logo from '../src/assets/img/theme/logo.png'
+import logo from "../src/assets/img/theme/logo.png";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-import { AuthContext, AuthProvider, TAuthConfig } from "react-oauth2-code-pkce"
-import { Constants } from './Environment';
-import CompanyDetail from 'views/examples/CompanyDetail';
-import GovtAgencyDetail from 'views/examples/GovtAgencyDetail';
-import { getUserData } from 'APIstore/apiCalls';
-import Profile from 'views/examples/Profile';
-import AllUsers from 'views/examples/AllUsers';
-import CorpDetail from 'views/examples/CorpDetail';
-import FleetDetail from 'views/examples/FleetDetail';
-import { fetchToken, onMessageListener } from '../src/Service/firebase';
-import reportWebVitals from '../src/Service/reportWebVitals';
-import Notifications, {notify} from 'react-notify-toast';
-import NotificationsComponent from 'component/NotificationsComponent';
-import config from 'config';
+import { AuthContext, AuthProvider, TAuthConfig } from "react-oauth2-code-pkce";
+import { Constants } from "./Environment";
+import CompanyDetail from "views/examples/CompanyDetail";
+import GovtAgencyDetail from "views/examples/GovtAgencyDetail";
+import { getUserData } from "APIstore/apiCalls";
+import Profile from "views/examples/Profile";
+import AllUsers from "views/examples/AllUsers";
+import CorpDetail from "views/examples/CorpDetail";
+import FleetDetail from "views/examples/FleetDetail";
+import { fetchToken, onMessageListener } from "../src/Service/firebase";
+import reportWebVitals from "../src/Service/reportWebVitals";
+import Notifications, { notify } from "react-notify-toast";
+import NotificationsComponent from "component/NotificationsComponent";
+import config from "config";
 console.warn(`${Constants.authUrl}/oauth2/authorize`);
 const authConfig: TAuthConfig = {
-  clientId: 'etows-v2-AM4pQ0fCQoI3Dr7meIRR',
+  // clientId: "etows-v1-HZYsxFmrQvi9H9r3RHtw",
+  clientId: "etows-v2-AM4pQ0fCQoI3Dr7meIRR",
   authorizationEndpoint: `${Constants.authUrl}/oauth2/authorize`,
   tokenEndpoint: `${Constants.authUrl}/oauth2/token`,
   // Whereever your application is running. Must match configuration on authorization server
   redirectUri: Constants.url,
   // Optional
-  scope: 'openid',
+  scope: "openid",
   // Optional
-  logoutEndpoint: '',
+  logoutEndpoint: "",
   // Optional
-  logoutRedirect: '',
+  logoutRedirect: "",
   // Example to redirect back to original path after login has completed
-  preLogin: () => localStorage.setItem('preLoginPath', window.location.pathname),
-  postLogin: () => window.location.replace(localStorage.getItem('preLoginPath')),
-  decodeToken: true
-}
+  preLogin: () =>
+    localStorage.setItem("preLoginPath", window.location.pathname),
+  postLogin: () =>
+    window.location.replace(localStorage.getItem("preLoginPath")),
+  decodeToken: true,
+};
 function LoginInfo(props) {
-  const navigate = useHistory()
+  const navigate = useHistory();
   // const location = useLocation();
-  const { tokenData, token, idToken, logOut, error, login } = useContext(AuthContext)
+  const { tokenData, token, idToken, logOut, error, login } =
+    useContext(AuthContext);
   useEffect(() => {
     async function myfetch() {
       if (token) {
-        await localStorage.setItem('myData', tokenData);
-        await localStorage.setItem('token', token);
-        navigate.push('/admin/index')
+        await localStorage.setItem("myData", tokenData);
+        await localStorage.setItem("token", token);
+        navigate.push("/admin/index");
       }
     }
-    myfetch()
-  }, [])
+    myfetch();
+  }, []);
   useEffect(() => {
-
     async function myfetchData() {
-
-      const token = await localStorage.getItem('token')
+      const token = await localStorage.getItem("token");
 
       if (token) {
-        navigate.push('/admin/index');
+        navigate.push("/admin/index");
       }
     }
-    myfetchData()
-  }, [])
+    myfetchData();
+  }, []);
   useEffect(() => {
-    loadGoogleMapScript()
-    if("serviceWorker" in navigator){
-      window.addEventListener("load",()=>{
-        navigator.serviceWorker.register("/firebase-messaging-sw.js")
-        .then((reg)=>{
-          console.log("Registration successful, scope is:",reg.scope)
-        }).catch((err)=>{
-          console.log("Service worker registration failed, error:",err)
-        })
-      })
+    loadGoogleMapScript();
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/firebase-messaging-sw.js")
+          .then((reg) => {
+            console.log("Registration successful, scope is:", reg.scope);
+          })
+          .catch((err) => {
+            console.log("Service worker registration failed, error:", err);
+          });
+      });
     }
-    
-  }, [])
+  }, []);
   const loadGoogleMapScript = useCallback((callback) => {
     const googleMapScript = document.createElement("script");
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_MAP_API_KEY}&libraries=places`;
@@ -90,33 +99,31 @@ function LoginInfo(props) {
   }, []);
   return (
     <div class="container h-100">
-
       <div class="row h-100">
         <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
           <div class="d-table-cell align-middle">
-
             <div class="text-center mt-4">
               <h1 class="h2">Welcome back to eTOWS</h1>
-              <p class="lead">
-                Sign in to your account to continue
-              </p>
+              <p class="lead">Sign in to your account to continue</p>
             </div>
 
-            <div style={{
-              position: "relative",
-              flexDirection: "column",
-              flex: 1,
-              minWidth: 0,
-              backgroundColor: "#fff",
-              borderWidth: 1,
-              borderColor: "#e5e9f2",
-              borderRadius: 10,
-              wordBreak: "break-word",
-              marginBottom: 24,
-              boxShadow: "rgb(217 189 182 / 34%) 1px 2px 9px",
-              margin: '4em',
-              padding: '1em',
-            }}>
+            <div
+              style={{
+                position: "relative",
+                flexDirection: "column",
+                flex: 1,
+                minWidth: 0,
+                backgroundColor: "#fff",
+                borderWidth: 1,
+                borderColor: "#e5e9f2",
+                borderRadius: 10,
+                wordBreak: "break-word",
+                marginBottom: 24,
+                boxShadow: "rgb(217 189 182 / 34%) 1px 2px 9px",
+                margin: "4em",
+                padding: "1em",
+              }}
+            >
               <div class="card-body">
                 <div class="m-sm-4">
                   <div class="text-center">
@@ -125,7 +132,15 @@ function LoginInfo(props) {
                   <form>
                     <div class="text-center mt-6" style={{ width: "100%" }}>
                       {/* <a href="https://etows.app:8443/oauth2/authorize" style={{ width: "100%", backgroundColor: 'green', border: 0 }} class="btn btn-lg btn-primary">Login</a> */}
-                      <button style={{ width: "100%", backgroundColor: 'green', border: 0 }} class="btn btn-lg btn-primary" onClick={() => login()}>
+                      <button
+                        style={{
+                          width: "100%",
+                          backgroundColor: "green",
+                          border: 0,
+                        }}
+                        class="btn btn-lg btn-primary"
+                        onClick={() => login()}
+                      >
                         Login
                       </button>
                     </div>
@@ -137,17 +152,15 @@ function LoginInfo(props) {
         </div>
       </div>
     </div>
-  )
-
+  );
 }
 ReactDOM.render(
-
   <AuthProvider authConfig={authConfig}>
-    <Notifications options={{zIndex: 200, top: '30px'}}/>
+    <Notifications options={{ zIndex: 200, top: "30px" }} />
     <NotificationsComponent />
     <BrowserRouter>
       <Switch>
-        <Route path="/admin" render={props => <AdminLayout {...props} />} />
+        <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
         <Route path="/admin/company-detail" element={<CompanyDetail />} />
         <Route path="/admin/agency-detail" element={<GovtAgencyDetail />} />
         <Route path="/admin/corp-documents-detail" element={<CorpDetail />} />
@@ -180,8 +193,6 @@ reportWebVitals();
 //   </AuthProvider>,
 //   document.getElementById("root")
 // );
-
-
 
 // const container = document.getElementById('root');
 // const root = createRoot(container); // createRoot(container!) if you use TypeScript
