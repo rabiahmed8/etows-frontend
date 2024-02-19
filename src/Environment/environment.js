99ecd1774fb3ec89551e3e1420f1ffb4c17b91e8
Rabi Ahmed <ahmedrabi8@gmail.com>
2024-02-19 21:55:48 +0500

    environment.js

--- src/Environment/environment.js
+++ src/Environment/environment.js
@@ -11,16 +11,18 @@
 // };
 
 // stage
+
 const Constants = {
   authUrl: "https://stage-api.etows.app:8443",
   apiUrl: "https://stage-api.etows.app:9000",
   url: "https://stage-console.etows.app",
 };
 
+// Local
+
 // const Constants = {
 //   authUrl: "http://192.168.0.145:8080",
 //   apiUrl: "http://192.168.0.145:9000",
-//   // url: "https://c3a2-124-29-238-60.ngrok-free.app:3000",
 //   url: "http://127.0.0.1:3000",
 // };
 
